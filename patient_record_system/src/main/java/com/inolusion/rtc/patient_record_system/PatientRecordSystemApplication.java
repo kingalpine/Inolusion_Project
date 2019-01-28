5b73ddd5e51d8fbacb030dca423dc89934460d56
wizrad <dppearsall97@gmail.com>
2019-01-27 18:16:45 -0600

    New stuff wasn't included in last commit

---
+++ patient_record_system/src/main/java/com/inolusion/rtc/patient_record_system/PatientRecordSystemApplication.java
@@ -0,0 +1,14 @@
+package com.inolusion.rtc.patient_record_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRecordSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientRecordSystemApplication.class, args);
+    }
+
+}
+
---
+++ patient_record_system/src/test/java/com/inolusion/rtc/patient_record_system/PatientRecordSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package com.inolusion.rtc.patient_record_system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatientRecordSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
