c51da087ed98ee263f229757cec276ffeb71b5b7
wizrad <dppearsall97@gmail.com>
2019-01-28 18:23:55 -0600

    Added index.css and updated index.html

---
+++ patient_record_system/src/main/java/com/inolusion/rtc/patient_record_system/PageController.java
@@ -0,0 +1,22 @@
+package com.inolusion.rtc.patient_record_system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/greeting")
+    public String showIndexPage(){
+    return "greeting";
+    }
+
+    @GetMapping("/patient_records")
+    public String showPatientRecordsPage() {
+        return "patient_records";
+    }
+    @GetMapping("/treatment_plan_progress")
+    public String showTreatmentPlanProgressPage(){
+        return "treatment_plan_progress";
+    }
+}
